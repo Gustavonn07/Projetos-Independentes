7784e1d839b2f98cffa3a97f5f5b480197f95ebc
Gustavonn07 <gustavonepnog@gmail.com>
2023-05-18 07:44:39 -0300

    Tou com fome

---
+++ Contador (HTML - CSS - JAVASCRIPT)/script.js
@@ -0,0 +1,14 @@
+// const num = 0;
+// let plus =+1; 
+// let minus =-1; 
+
+// document.querySelector()
+
+// function calc() {
+//     if (+1) {
+//     num =+ 1;
+//     }
+//     else {
+//     num =-1;
+//     }
+// }
